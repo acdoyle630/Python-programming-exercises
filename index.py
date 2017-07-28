d0f1886ff65b48fcf017606b038dc6028daf102c
Adam <acdoyle630@gmail.com>
2017-07-27 16:51:46 -1000

    number 11

--- index.py
+++ index.py
@@ -83,3 +83,40 @@ def wordSort(string):
   print stringArray
 
 wordSort('hello,how,are,you')
+
+# Question 9
+def cap(words):
+  return words.upper()
+
+print cap('fuck you')
+
+#Question 10
+def sortDuplicateCheck (words):
+  wordArray = words.split(' ')
+  print wordArray
+  my_list = []
+  for i in wordArray:
+    print i
+    if i in my_list:
+      print 'duplicate'
+    else:
+      my_list.append(i)
+  return my_list
+
+
+
+print sortDuplicateCheck('hello hello hello hi this is a test test')
+
+#Question 11
+def numberDivByFive(numberString):
+  numberArray = numberString.split(',')
+  numbersByFive = []
+  for i in numberArray:
+    if int(i) % 5 == 0:
+      numbersByFive.append(int(i))
+  return numbersByFive
+
+print numberDivByFive('1,2,3,4,5,6,7,8,10,15')
+
+
+numberDivByFive('10,13,14,15')
